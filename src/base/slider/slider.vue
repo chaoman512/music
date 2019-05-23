16a5f46ced4373bb900ae2a9778e5a2f78136a87
chaoman512 <1006718446@qq.com>
2019-05-24 02:41:21 +0800

    歌手名单

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,57 +1,91 @@
 <template>
   <div class="slider" ref="slider">
-  	<div class="slider-group" ref="sliderGroup">
-  		<slot></slot>
-  	</div>
-  	<div class="dots">
-  		
-  	</div>
+    <div class="slider-group" ref="sliderGroup">
+      <slot>
+      </slot>
+    </div>
+    <div class="dots">
+      <span class="dot" :class="{active: currentPageIndex === index }" v-for="(item, index) in dots"></span>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import BScroll from "better-scroll"
-import {addClass} from "common/js/dom"
+  import {addClass} from 'common/js/dom'
+  import BScroll from 'better-scroll'
+
   export default {
-  	props:{
-  		loop:{
-  			type:Boolean,
-  			default:true
-  		},
-  		autoPlay:{
-  			type:Boolean,
-  			default:true
-  		},
-  		interval:{
-  			type:Number,
-  			default:4000
-  		}
-  		},
-  	mounted(){
-  		setTimeout(()=>{
-         this._setSliderWidth()
-         this._initSlider()
-  		},20)
-  	},
-    methods:{
-    	_setSliderWidth(){
-          this.children=this.$refs.sliderGroup.children
-          let width=0
-          let sliderWidth=this.$refs.slider.clientWidth
-          for(let i=0;i<this.children.length;i++){
-          	let child=this.children[i]
-          	addClass(child,'slider-item')
-          	child.style.width=sliderWidth+'px'
-          	width+=sliderWidth
-          }
-       
-         if(this.loop){
-         	width+=2*sliderWidth
-         }
-         this.$refs.sliderGroup.style.width=width+'px'
-    	},
-    	_initSlider(){
-          this.slider = new BScroll(this.$refs.slider, {
+    name: 'slider',
+    props: {
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      autoPlay: {
+        type: Boolean,
+        default: true
+      },
+      interval: {
+        type: Number,
+        default: 4000
+      }
+    },
+    data() {
+      return {
+        dots: [],
+        currentPageIndex: 0
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._setSliderWidth()
+        this._initDots()
+        this._initSlider()
+
+        if (this.autoPlay) {
+          this._play()
+        }
+      }, 20)
+
+      window.addEventListener('resize', () => {
+        if (!this.slider) {
+          return
+        }
+        this._setSliderWidth(true)
+        this.slider.refresh()
+      })
+    },
+    activated() {
+      if (this.autoPlay) {
+        this._play()
+      }
+    },
+    deactivated() {
+      clearTimeout(this.timer)
+    },
+    beforeDestroy() {
+      clearTimeout(this.timer)
+    },
+    methods: {
+      _setSliderWidth(isResize) {
+        this.children = this.$refs.sliderGroup.children
+
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slider-item')
+
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+        }
+        if (this.loop && !isResize) {
+          width += 2 * sliderWidth
+        }
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initSlider() {
+        this.slider = new BScroll(this.$refs.slider, {
           scrollX: true,
           scrollY: false,
           momentum: false,
@@ -60,15 +94,43 @@ import {addClass} from "common/js/dom"
           snapThreshold: 0.3,
           snapSpeed: 400
         })
-    	}
-    }
 
-  	}
+        this.slider.on('scrollEnd', () => {
+          let pageIndex = this.slider.getCurrentPage().pageX
+          if (this.loop) {
+            pageIndex -= 1
+          }
+          this.currentPageIndex = pageIndex
+
+          if (this.autoPlay) {
+            this._play()
+          }
+        })
+
+        this.slider.on('beforeScrollStart', () => {
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+          }
+        })
+      },
+      _initDots() {
+        this.dots = new Array(this.children.length)
+      },
+      _play() {
+        let pageIndex = this.currentPageIndex + 1
+        if (this.loop) {
+          pageIndex += 1
+        }
+        this.timer = setTimeout(() => {
+          this.slider.goToPage(pageIndex, 0, 400)
+        }, this.interval)
+      }
+    }
+  }
 </script>
 
-
 <style scoped lang="stylus" rel="stylesheet/stylus">
-    @import "~common/stylus/variable"
+  @import "~common/stylus/variable"
 
   .slider
     min-height: 1px
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,19 +1,38 @@
 <template>
-        <div id="">
-   singer
-        </div>
+      <div class="singer"></div>
 </template>
 
 
 <script type="text/ecmascript-6">
+import {getSingerList} from "api/singer"
+import {ERR_OK} from "api/config"
 export default {
-  name: '',
-  components: {
-  }
+    data(){
+    	return{
+    		singers:[]
+    	}
+    },
+    created(){
+      this._getSingerList()
+    },
+    methods:{
+     _getSingerList(){
+     	getSingerList().then((res)=>{
+     		if(res.code==ERR_OK){
+     			this.singers=res.data.list
+     		}
+     	})
+     },
+    }
 }
 </script>
 
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+.singer
+  position:fixed
+  top:80px
+  width:100%
+  bottom:0px
 
 </style>
